93949239c9b16f4d558b49c651afa8a060f70dd7
Meine Toonen <toonen.meine@gmail.com>
2017-05-16 09:38:58 +0200

    Moving functions and files

--- src/main/java/nl/b3p/playbase/stripes/ImportPlaymappingActionBean.java
+++ src/main/java/nl/b3p/playbase/stripes/ImportPlaymappingActionBean.java
@@ -21,6 +21,7 @@ import net.sourceforge.stripes.action.StrictBinding;
 import net.sourceforge.stripes.action.UrlBinding;
 import net.sourceforge.stripes.validation.SimpleError;
 import net.sourceforge.stripes.validation.Validate;
+import nl.b3p.playbase.PlaymappingProcessor;
 import nl.b3p.playbase.db.DB;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -73,6 +74,8 @@ public class ImportPlaymappingActionBean implements ActionBean {
     private String password;
     @Validate(required = true)
     private String apiurl;
+    
+    private final PlaymappingProcessor processor = new PlaymappingProcessor();
 
     @DefaultHandler
     @DontValidate
@@ -182,10 +185,10 @@ public class ImportPlaymappingActionBean implements ActionBean {
             int retval;
             String type;
             if (apiurl.contains("Location")) {
-                retval = refillLocationsApiTable(stringResult);
+                retval = processor.refillLocationsApiTable(stringResult);
                 type = "locaties";
             } else if (apiurl.contains("Asset")) {
-                retval = refillAssetsApiTable(stringResult);
+                retval = processor.refillAssetsApiTable(stringResult);
                 type = "assets";
             } else {
                 context.getValidationErrors().add("apiurl", new SimpleError("Wrong url selected."));
@@ -194,201 +197,6 @@ public class ImportPlaymappingActionBean implements ActionBean {
             context.getMessages().add(new SimpleMessage("Er zijn " + retval + " " + type + " weggeschreven."));
         }
         return new ForwardResolution(JSP);
-    }
-
-    private int refillAssetsApiTable(String assetsString) throws NamingException, SQLException {
-        StringBuilder sb;
-        List<Map<String, Object>> assets = parseAssets(assetsString);
-        int retval = 0;
-        for (Map<String, Object> asset : assets) {
-            sb = new StringBuilder();
-            sb.append("insert into pm_assets_api (");
-            sb.append("	 \"id\",");
-            sb.append("      \"locationid\",");
-            sb.append("      \"locationname\",");
-            sb.append("      \"lastupdated\",");
-            sb.append("      \"name\",");
-            sb.append("      \"assettype\",");
-            sb.append("      \"manufacturer\",");
-            sb.append("      \"serialnumber\",");
-            sb.append("      \"installeddate\",");
-            sb.append("      \"endoflifeyear\",");
-            sb.append("      \"safetyzonelength\",");
-            sb.append("      \"safetyzonewidth\",");
-            sb.append("      \"agegrouptoddlers\",");
-            sb.append("      \"agegroupjuniors\",");
-            sb.append("      \"agegroupseniors\",");
-            sb.append("      \"pricepurchase\",");
-            sb.append("      \"priceinstallation\",");
-            sb.append("      \"pricereinvestment\",");
-            sb.append("      \"pricemaintenance\",");
-            sb.append("      \"priceindexation\",");
-            sb.append("      \"lat\",");
-            sb.append("      \"lng\"");
-            sb.append(") ");
-            sb.append("VALUES (");
-            sb.append("'").append(asset.get("ID")).append("',");
-            sb.append("'").append(asset.get("LocationID")).append("',");
-            sb.append("'").append(asset.get("LocationName")).append("',");
-            sb.append("'").append(asset.get("LastUpdated")).append("',");
-            sb.append("'").append(asset.get("Name")).append("',");
-            sb.append("'").append(asset.get("AssetType")).append("',");
-            sb.append("'").append(asset.get("Manufacturer")).append("',");
-            sb.append("'").append(asset.get("SerialNumber")).append("',");
-            sb.append("'").append(asset.get("InstalledDate")).append("',");
-            sb.append("'").append(asset.get("EndOfLifeYear")).append("',");
-            sb.append("'").append(asset.get("SafetyZoneLength")).append("',");
-            sb.append("'").append(asset.get("SafetyZoneWidth")).append("',");
-            sb.append("'").append(asset.get("AgeGroupToddlers")).append("',");
-            sb.append("'").append(asset.get("AgeGroupJuniors")).append("',");
-            sb.append("'").append(asset.get("AgeGroupSeniors")).append("',");
-            sb.append("'").append(asset.get("PricePurchase")).append("',");
-            sb.append("'").append(asset.get("PriceInstallation")).append("',");
-            sb.append("'").append(asset.get("PriceReInvestment")).append("',");
-            sb.append("'").append(asset.get("PriceMaintenance")).append("',");
-            sb.append("'").append(asset.get("PriceIndexation")).append("',");
-            sb.append("'").append(asset.get("Lat")).append("',");
-            sb.append("'").append(asset.get("Lng")).append("');");
-            retval += DB.qr().update(sb.toString());
-        }
-        return retval;
-    }
-
-    private int refillLocationsApiTable(String temp) throws NamingException, SQLException {
-        List<Map<String, Object>> childLocations = parseChildLocations(temp);
-        int retval = 0;
-        for (Map<String, Object> childLocation : childLocations) {
-            //insert childlocations from childlocations from childlocations
-            StringBuilder sb = new StringBuilder();
-            sb.append("INSERT ");
-            sb.append("INTO ");
-            sb.append("    pm_locations_api ");
-            sb.append("    (");
-            sb.append("        \"id\",");
-            sb.append("        \"name\",");
-            sb.append("        \"lastupdated\",");
-            sb.append("        \"lat\",");
-            sb.append("        \"lng\"");
-            sb.append("    )");
-            sb.append("    VALUES(");
-            sb.append("    ");
-            sb.append("\'").append(childLocation.get("ID")).append("\',");
-            sb.append("\'").append(childLocation.get("LastUpdated")).append("\',");
-            sb.append("\'").append(childLocation.get("Name")).append("\',");
-            sb.append("").append(childLocation.get("Lat")).append(",");
-            sb.append("").append(childLocation.get("Lng")).append("");
-            sb.append(");");
-            retval += DB.qr().update(sb.toString());
-        }
-        return retval;
-    }
-
-    protected List<Map<String, Object>> parseAssets(String assetsString) {
-        List<Map<String, Object>> assets = new ArrayList<>();
-        JSONArray assetsArray = new JSONArray(assetsString);
-
-        for (int i = 0; i < assetsArray.length(); i++) {
-            JSONObject asset = assetsArray.getJSONObject(i);
-            assets.add(parseAsset(asset));
-
-        }
-        return assets;
-    }
-
-    protected Map<String, Object> parseAsset(JSONObject assetJSON) {
-        Map<String, Object> asset = new HashMap<>();
-        asset.put("$id", assetJSON.optString("$id"));
-        asset.put("ID", assetJSON.optString("ID"));
-        asset.put("LocationID", assetJSON.optString("LocationID"));
-        asset.put("LocationName", assetJSON.optString("LocationName").replaceAll("\'", "\'\'"));
-        asset.put("LastUpdated", assetJSON.optString("LastUpdated"));
-        asset.put("Name", assetJSON.optString("Name").replaceAll("\'", "\'\'"));
-        asset.put("AssetType", assetJSON.optString("AssetType"));
-        asset.put("Manufacturer", assetJSON.optString("Manufacturer"));
-        asset.put("Product", assetJSON.optString("Product"));
-        asset.put("SerialNumber", assetJSON.optString("SerialNumber"));
-        asset.put("Material", assetJSON.optString("Material"));
-        asset.put("InstalledDate", assetJSON.optString("InstalledDate"));
-        asset.put("EndOfLifeYear", assetJSON.optInt("EndOfLifeYear"));
-        asset.put("ProductID", assetJSON.optString("ProductID"));
-        asset.put("ProductVariantID", assetJSON.optString("ProductVariantID"));
-        asset.put("Height", assetJSON.optInt("Height"));
-        asset.put("Depth", assetJSON.optInt("Depth"));
-        asset.put("Width", assetJSON.optInt("Width"));
-        asset.put("FreefallHeight", assetJSON.optInt("FreefallHeight"));
-        asset.put("SafetyZoneLength", assetJSON.optInt("SafetyZoneLength"));
-        asset.put("SafetyZoneWidth", assetJSON.optInt("SafetyZoneWidth"));
-        asset.put("AgeGroupToddlers", assetJSON.optBoolean("AgeGroupToddlers"));
-        asset.put("AgeGroupJuniors", assetJSON.optBoolean("AgeGroupJuniors"));
-        asset.put("AgeGroupSeniors", assetJSON.optBoolean("AgeGroupSeniors"));
-        asset.put("PricePurchase", assetJSON.optDouble("PricePurchase"));
-        asset.put("PriceInstallation", assetJSON.optDouble("PriceInstallation"));
-        asset.put("PriceReInvestment", assetJSON.optDouble("PriceReInvestment"));
-        asset.put("PriceMaintenance", assetJSON.optDouble("PriceMaintenance"));
-        asset.put("PriceIndexation", assetJSON.optDouble("PriceIndexation"));
-        asset.put("Lat", Double.parseDouble(assetJSON.optString("Lat").replaceAll(",", ".")));
-        asset.put("Lng", Double.parseDouble(assetJSON.optString("Lng").replaceAll(",", ".")));
-        asset.put("Documents", assetJSON.optJSONArray("Documents"));
-        asset.put("Hyperlinks", assetJSON.optJSONArray("Hyperlinks"));
-        asset.put("Images", parseImages(assetJSON.optJSONArray("Images")));
-        return asset;
-    }
-
-    protected List<Map<String, Object>> parseChildLocations(String locations) {
-        List<Map<String, Object>> locs = new ArrayList<>();
-        JSONArray childLocations = new JSONArray(locations);
-        for (int i = 0; i < childLocations.length(); i++) {
-            JSONObject childLocation = childLocations.getJSONObject(i);
-            JSONArray cls = childLocation.getJSONArray("ChildLocations");
-            if (cls.length() == 0) {
-                locs.add(parseLocation(childLocation));
-            } else {
-                locs.addAll(parseChildLocations(cls.toString()));
-            }
-
-        }
-        return locs;
-    }
-
-    protected Map<String, Object> parseLocation(JSONObject json) {
-        Map<String, Object> location = new HashMap<>();
-        location.put("$id", json.optString("$id"));
-        location.put("ID", json.optString("ID"));
-        location.put("LastUpdated", json.optString("LastUpdated"));
-        location.put("Name", json.optString("Name").replaceAll("\'", "\'\'"));
-        location.put("AddressLine1", json.optString("AddressLine1"));
-        location.put("Suburb", json.optString("Suburb"));
-        location.put("City", json.optString("City"));
-        location.put("Area", json.optString("Area"));
-        location.put("PostCode", json.optString("PostCode"));
-        location.put("Ref", json.optString("Ref"));
-        location.put("AssetCount", json.optInt("AssetCount"));
-        location.put("Lat", Double.parseDouble(json.optString("Lat").replaceAll(",", ".")));
-        location.put("Lng", Double.parseDouble(json.optString("Lng").replaceAll(",", ".")));
-        location.put("ChildLocations", json.optJSONArray("ChildLocations"));
-        location.put("Documents", json.optJSONArray("Documents"));
-        location.put("Images", parseImages(json.optJSONArray("Images")));
-        return location;
-    }
-
-    protected List<Map<String, Object>> parseImages(JSONArray images) {
-        List<Map<String, Object>> imagesList = new ArrayList<>();
-        for (int i = 0; i < images.length(); i++) {
-            JSONObject img = images.getJSONObject(i);
-            Map<String, Object> image = parseImage(img);
-            imagesList.add(image);
-        }
-        return imagesList;
-    }
-
-    protected Map<String, Object> parseImage(JSONObject image) {
-        Map<String, Object> imageMap = new HashMap<>();
-        imageMap.put("$id", image.optString("$id"));
-        imageMap.put("ID", image.optString("ID"));
-        imageMap.put("LastUpdated", image.optString("LastUpdated"));
-        imageMap.put("URI", image.optString("URI"));
-        imageMap.put("Description", image.optString("Description"));
-        return imageMap;
     }
 
     // <editor-fold desc="Getters and setters" defaultstate="collapsed">
---
+++ src/test/java/nl/b3p/playbase/PlaymappingProcessorTest.java
@@ -0,0 +1,123 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nl.b3p.playbase;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Map;
+import nl.b3p.playbase.stripes.ImportPlaymappingActionBeanTest;
+import org.apache.commons.io.IOUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Before;
+
+/**
+ *
+ * @author meine
+ */
+public class PlaymappingProcessorTest {
+    
+    public PlaymappingProcessorTest() {
+    }
+
+    public PlaymappingProcessor instance;
+    
+    @Before
+    public void beforeTest(){
+        instance =  new PlaymappingProcessor();
+    }
+   
+    @Test
+    public void testGetChildLocation3Levels() throws IOException{
+        InputStream in = ImportPlaymappingActionBeanTest.class.getResourceAsStream("pijnacker3Locations.json");
+        String location = IOUtils.toString(in);
+        List<Map<String,Object>> returnValue = instance.parseChildLocations(location);
+        assertEquals(195, returnValue.size());
+    }
+    
+    @Test
+    public void testGetChildLocation2Levels() throws IOException{
+        InputStream in = ImportPlaymappingActionBeanTest.class.getResourceAsStream("haarlemLocations.json");
+        String location = IOUtils.toString(in);
+        List<Map<String,Object>> returnValue = instance.parseChildLocations(location);
+        assertEquals(273, returnValue.size());
+    }
+    
+    @Test
+    public void testParseLocation(){
+        String loc = "{\"$id\": \"3\",\"ID\": \"98d66e1b-e2eb-44ea-ab1b-ce5999cb4309\",\"LastUpdated\": \"2015-01-08T11:18:10.613\",\"Name\": \"C117/1009 Wilsonplein 8\",\"AddressLine1\": \"Wilsonplein 8\\r\\nHaarlem\",\"Suburb\": \"\",\"City\": \"\",\"Area\": \"\",\"PostCode\": \"\",\"Ref\": \"C117/1009\",\"AssetCount\": 14,\"Lat\": \"52,37759\",\"Lng\": \"4,627348\",\"ChildLocations\": [],\"Images\": [  {    \"$id\": \"4\",    \"ID\": \"33ec50eb-eec8-452a-bf7c-1b54ca7543f8\",    \"LastUpdated\": \"2012-08-02T07:44:06\",    \"URI\": \"http://www.playmapping.com/GetImage.ashx?g=33ec50eb-eec8-452a-bf7c-1b54ca7543f8&w=350&h=350\",    \"Description\": \"\"  },  {    \"$id\": \"5\",    \"ID\": \"8af2bd67-5178-405f-9a97-444133bf370d\",    \"LastUpdated\": \"2012-08-02T07:44:37\",    \"URI\": \"http://www.playmapping.com/GetImage.ashx?g=8af2bd67-5178-405f-9a97-444133bf370d&w=350&h=350\",    \"Description\": \"\"  }],\"Documents\": []}";
+        JSONObject location = new JSONObject (loc);
+        Map<String, Object> real = instance.parseLocation(location);
+        assertEquals("3", real.get("$id"));
+        assertEquals("98d66e1b-e2eb-44ea-ab1b-ce5999cb4309", real.get("ID"));
+        assertEquals("2015-01-08T11:18:10.613", real.get("LastUpdated"));
+        assertEquals("C117/1009 Wilsonplein 8", real.get("Name"));
+        assertEquals(52.37759, real.get("Lat"));
+        assertEquals(4.627348, real.get("Lng"));
+        assertEquals("Wilsonplein 8\r\nHaarlem", real.get("AddressLine1"));
+        assertEquals("", real.get("Suburb"));
+        assertEquals("", real.get("City"));
+        assertEquals("", real.get("Area"));
+        assertEquals("", real.get("PostCode"));
+        assertEquals("C117/1009", real.get("Ref"));
+        assertEquals(14, real.get("AssetCount"));
+        assertEquals(0, ((JSONArray)real.get("ChildLocations")).length());
+        assertEquals(0, ((JSONArray)real.get("Documents")).length());
+        assertEquals(2, ((List)real.get("Images")).size());
+    }
+    
+    @Test
+    public void testParseAssets() throws IOException {
+        InputStream in = ImportPlaymappingActionBeanTest.class.getResourceAsStream("haarlemAssets.json");
+        String location = IOUtils.toString(in);
+        List<Map<String, Object>> returnValue = instance.parseAssets(location);
+        assertEquals(2339, returnValue.size());
+    }
+    
+    @Test
+    public void testParseAsset(){
+        String assetString = "{\"$id\": \"1\",\"ID\": \"dc5d0399-ccc4-4c22-9ab0-2a7b9ff80a19\",\"LocationID\": \"0d415c38-ca18-4d67-a716-00df56df8736\",\"LocationName\": \"Centrum\\\\C136/1005 Vroonhof 1\",\"LastUpdated\": \"2013-01-29T11:24:59.25\",\"Name\": \"Motorfiets\",\"AssetType\": \"Wiptoestellen/Type 2A - Enkelpunts - 1 richting\",\"Manufacturer\": \"KOMPAN A/S\",\"Product\": \"MOMENTS\\\\M130P\",\"SerialNumber\": \"04\",\"Material\": \"\",\"InstalledDate\": \"1988-01-01\",\"EndOfLifeYear\": -1,\"ProductID\": \"eeb6cc3b-0f77-40ac-8926-09588625244d\",\"ProductVariantID\": \"0ff2d747-6634-4ac5-861d-697ab4d80762\",\"Height\": 820,\"Depth\": 900,\"Width\": 360,\"FreefallHeight\": 560,\"SafetyZoneLength\": 3500,\"SafetyZoneWidth\": 2360,\"AgeGroupToddlers\": true,\"AgeGroupJuniors\": false,\"AgeGroupSeniors\": false,\"PricePurchase\": 700.0,\"PriceInstallation\": -1.0,\"PriceReInvestment\": -1.0,\"PriceMaintenance\": -1.0,\"PriceIndexation\": -1.0,\"Lat\": \"52,38138\",\"Lng\": \"4,641622\",\"Images\": [{\"$id\": \"3\",\"ID\": \"7f8ac724-1821-4bee-8f34-f8894deb5cac\",\"LastUpdated\": \"2010-07-06T18:15:08.453\",\"URI\": \"http://www.playmapping.com/GetImage.ashx?g=7f8ac724-1821-4bee-8f34-f8894deb5cac&w=350&h=350\",\"Description\": \"\"}],\"Documents\": [],\"Hyperlinks\": [],\"LinkedAssets\": []}";
+        JSONObject assetJSON = new JSONObject(assetString);
+        Map<String, Object> map = instance.parseAsset(assetJSON);
+        assertEquals("1", map.get("$id"));
+        assertEquals("dc5d0399-ccc4-4c22-9ab0-2a7b9ff80a19", map.get("ID"));
+        assertEquals("0d415c38-ca18-4d67-a716-00df56df8736", map.get("LocationID"));
+        assertEquals("Centrum\\C136/1005 Vroonhof 1", map.get("LocationName"));
+        assertEquals("2013-01-29T11:24:59.25", map.get("LastUpdated"));
+        assertEquals("Motorfiets", map.get("Name"));
+        assertEquals("Wiptoestellen/Type 2A - Enkelpunts - 1 richting", map.get("AssetType"));
+        assertEquals("KOMPAN A/S", map.get("Manufacturer"));
+        assertEquals("MOMENTS\\M130P", map.get("Product"));
+        assertEquals("04", map.get("SerialNumber"));
+        assertEquals("", map.get("Material"));
+        assertEquals("1988-01-01", map.get("InstalledDate"));
+        assertEquals(-1, map.get("EndOfLifeYear"));
+        assertEquals("eeb6cc3b-0f77-40ac-8926-09588625244d", map.get("ProductID"));
+        assertEquals("0ff2d747-6634-4ac5-861d-697ab4d80762", map.get("ProductVariantID"));
+        assertEquals(820, map.get("Height"));
+        assertEquals(900, map.get("Depth"));
+        assertEquals(360, map.get("Width"));
+        assertEquals(560, map.get("FreefallHeight"));
+        assertEquals(3500, map.get("SafetyZoneLength"));
+        assertEquals(2360, map.get("SafetyZoneWidth"));
+        assertEquals(true, map.get("AgeGroupToddlers"));
+        assertEquals(false, map.get("AgeGroupJuniors"));
+        assertEquals(false, map.get("AgeGroupSeniors"));
+        assertEquals(700.0, map.get("PricePurchase"));
+        assertEquals(-1.0, map.get("PriceInstallation"));
+        assertEquals(-1.0, map.get("PriceReInvestment"));
+        assertEquals(-1.0, map.get("PriceMaintenance"));
+        assertEquals(-1.0, map.get("PriceIndexation"));
+        assertEquals(52.38138, map.get("Lat"));
+        assertEquals(4.641622, map.get("Lng"));
+        assertEquals(0, ((JSONArray)map.get("Documents")).length());
+        assertEquals(1, ((List)map.get("Images")).size());
+        assertEquals(0, ((JSONArray)map.get("Hyperlinks")).length());
+    }
+}
