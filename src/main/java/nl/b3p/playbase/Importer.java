cf2d38d695e6f32724deb742da57545cc150f682
Meine Toonen <toonen.meine@gmail.com>
2017-09-12 10:52:28 +0200

    Save address fields

--- src/main/java/nl/b3p/playbase/Importer.java
+++ src/main/java/nl/b3p/playbase/Importer.java
@@ -184,14 +184,17 @@ public abstract class Importer {
             sb.append("content,");
             sb.append("municipality,");
             sb.append("country,");
+            sb.append("street,");
+            sb.append("postalcode,");
+            sb.append("phone,");
             sb.append("pa_id,");
             sb.append("pa_title,");
             sb.append("pm_guid) ");
-            sb.append("VALUES( ?,?,?,?,?,?,?,?,?,?,?);");
+            sb.append("VALUES( ?,?,?,?,?,?,?,?,?,?,?,?,?,?);");
 
             savedLocation = DB.qr().insert(sb.toString(), handler, location.getTitle(), location.getLatitude(), location.getLongitude(), geom, 
                     location.getAveragerating() != null ? location.getAveragerating() : 0, location.getContent(), location.getMunicipality(), location.getCountry(),
-                    location.getPa_id(), location.getPa_title(), location.getPm_guid());
+                   location.getStreet(), location.getPostalcode(), location.getPhone(), location.getPa_id(), location.getPa_title(), location.getPm_guid());
             id = savedLocation.getId();
             report.increaseInserted(ImportType.LOCATION);
             List<Map<String, Object>> images = location.getImages();
@@ -211,6 +214,9 @@ public abstract class Importer {
             sb.append("content = ?,");
             sb.append("municipality = ?,");
             sb.append("country = ?,");
+            sb.append("street = ?,");
+            sb.append("postalcode= ?,");
+            sb.append("phone = ?,");
             sb.append("pa_id = ?,");
             sb.append("pa_title = ?,");
             sb.append("pm_guid = ?");
@@ -218,7 +224,7 @@ public abstract class Importer {
 
             DB.qr().update(sb.toString(), location.getTitle(), location.getLatitude(), location.getLongitude(), geom, 
                     location.getAveragerating() != null ? location.getAveragerating() : 0, location.getContent(), location.getMunicipality(), location.getCountry(),
-                    location.getPa_id(), location.getPa_title(), location.getPm_guid(), id);
+                    location.getStreet(), location.getPostalcode(), location.getPhone(), location.getPa_id(), location.getPa_title(), location.getPm_guid(), id);
             report.increaseUpdated(ImportType.LOCATION);
         }
         return id;
